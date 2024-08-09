019eb02dffef019c0de97f8124f4b9f8a5385c7c
Ital023 <contadoitalo911@gmail.com>
2024-08-08 22:38:08 -0300

    feat: Category Resource Find By Id

--- backend/src/main/java/io/github/ital023/dscatalog/services/CategoryService.java
+++ backend/src/main/java/io/github/ital023/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -21,5 +22,14 @@ public class CategoryService {
 
         return list.stream().map(CategoryDTO::new).toList();
     }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO getById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.get();
+
+        return new CategoryDTO(entity);
+    }
+
 
 }
