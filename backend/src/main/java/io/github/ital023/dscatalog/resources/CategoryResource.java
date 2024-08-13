aa4e180fdeb410cdfd8bba8f28a0491b37374061
Ital023 <contadoitalo911@gmail.com>
2024-08-12 22:50:17 -0300

    feat: Product entity, SQL script

--- backend/src/main/java/io/github/ital023/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/io/github/ital023/dscatalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import io.github.ital023.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
