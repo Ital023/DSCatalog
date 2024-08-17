51ddcfd9ced804f1cf7b3992cec704aaf06304cc
Ital023 <contadoitalo911@gmail.com>
2024-08-17 14:40:48 -0300

    test: Factory, save tests

--- backend/src/test/java/io/github/ital023/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/io/github/ital023/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,7 +1,10 @@
 package io.github.ital023.dscatalog.repositories;
 
 import io.github.ital023.dscatalog.entities.Product;
+import io.github.ital023.dscatalog.services.exceptions.ResourceNotFoundException;
+import io.github.ital023.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,16 +17,39 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
+    private long existId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existId = 1L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull(){
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExist(){
         //Arrange
-        long existId = 1L;
 
         //Act
         repository.deleteById(existId);
 
+        //Assert
         Optional<Product> result = repository.findById(existId);
         Assertions.assertFalse(result.isPresent());
     }
+
+
 
 }
