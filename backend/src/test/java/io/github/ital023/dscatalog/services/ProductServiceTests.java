498acf55e3ac40be54e0def6f45edc7b0fd95333
Ital023 <contadoitalo911@gmail.com>
2024-10-03 16:44:48 -0300

    test: initial code product mock tests

---
+++ backend/src/test/java/io/github/ital023/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,20 @@
+package io.github.ital023.dscatalog.services;
+
+import io.github.ital023.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+
+    @Mock
+    private ProductRepository productRepository;
+
+
+
+}
