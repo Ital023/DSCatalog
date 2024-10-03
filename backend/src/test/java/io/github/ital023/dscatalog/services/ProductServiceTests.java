ce949a3cc7de6b38891c83f7bd4de50a6a53d572
Ital023 <contadoitalo911@gmail.com>
2024-10-03 17:36:15 -0300

    test: mockito tests service

--- backend/src/test/java/io/github/ital023/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/io/github/ital023/dscatalog/services/ProductServiceTests.java
@@ -1,9 +1,13 @@
 package io.github.ital023.dscatalog.services;
 
 import io.github.ital023.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -15,6 +19,34 @@ public class ProductServiceTests {
     @Mock
     private ProductRepository productRepository;
 
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    void setUp() {
+        existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
+        Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
+        //Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
+
+
+        Mockito.doNothing().when(productRepository).deleteById(existingId);
+
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(()->{
+            service.delete(existingId);
+        });
+
+        Mockito.verify(productRepository, Mockito.times(1)).deleteById(existingId);
+    }
 
 
 }
+
+
+
