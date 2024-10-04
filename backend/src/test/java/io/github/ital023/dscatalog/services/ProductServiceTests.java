f6ad67f37fca62ef8470b9d0f9447ffd83bcc9d3
Ital023 <contadoitalo911@gmail.com>
2024-10-04 20:43:45 -0300

    test: dependent id

--- backend/src/test/java/io/github/ital023/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/io/github/ital023/dscatalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package io.github.ital023.dscatalog.services;
 
 import io.github.ital023.dscatalog.repositories.ProductRepository;
+import io.github.ital023.dscatalog.services.exceptions.DataBaseException;
 import io.github.ital023.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -22,19 +24,29 @@ public class ProductServiceTests {
 
     private Long existingId;
     private Long nonExistingId;
+    private Long dependentId;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
         nonExistingId = 1000L;
+        dependentId = 4L;
 
         Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
         Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
-        //Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
+        Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
 
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
 
+    }
+
+    @Test
+    void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        Assertions.assertThrows(DataBaseException.class, () -> {
+           service.delete(dependentId);
+        });
     }
 
     @Test
