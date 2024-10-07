4b70f7f40205729cae3f89f797bab9a7185cd063
Ital023 <contadoitalo911@gmail.com>
2024-10-07 13:58:13 -0300

    test: organization

--- backend/src/test/java/io/github/ital023/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/io/github/ital023/dscatalog/resources/ProductResourceTests.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.List;
@@ -41,9 +44,10 @@ public class ProductResourceTests {
 
     @Test
     void findAllShouldReturnPage() throws Exception{
-        mockMvc.perform(get("/products")).andExpect(status().isOk());
+        ResultActions result = mockMvc.perform(get("/products")
+                .accept(MediaType.APPLICATION_JSON));
 
-
+        result.andExpect(status().isOk());
     }
 
 }
