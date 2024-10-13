d98c526c41c5483b1d4ad1868769d3ed1e00e0f6
Ital023 <contadoitalo911@gmail.com>
2024-10-13 10:42:50 -0300

    feat: User dto / repository

---
+++ backend/src/main/java/io/github/ital023/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package io.github.ital023.dscatalog.repositories;
+
+import io.github.ital023.dscatalog.entities.Category;
+import io.github.ital023.dscatalog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
